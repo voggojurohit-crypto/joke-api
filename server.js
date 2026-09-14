70c3118acf2039cbe6995ad1350b76069995e215
Your Name <you@example.com>
2026-09-14 21:10:58 +0530

    Prepare app for Render deployment

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
-
-const API_URL = "http://localhost:4000";
+const port = process.env.PORT || 3000;
+const API_URL = process.env.API_URL || "http://localhost:4000";
 
 app.use(express.static("public"));
 
